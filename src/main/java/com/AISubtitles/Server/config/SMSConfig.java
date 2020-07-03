44abc79688c0dcb97d50a41c1a6da22816be082b
KiwiJuiceZ <43952178+KiwiJuiceZ@users.noreply.github.com>
2020-07-03 14:11:30 +0800

    Update SMSConfig.java

--- src/main/java/com/AISubtitles/Server/config/SMSConfig.java
+++ src/main/java/com/AISubtitles/Server/config/SMSConfig.java
@@ -10,12 +10,12 @@ public class SMSConfig {
 	/**
 	 * 开发者注册后系统自动生成的账号，可在官网登录后查看
 	 */
-	public static final String ACCOUNT_SID = "9bc8da24a8c21dd419588f8ca0da83a4";
+	public static final String ACCOUNT_SID = "";
 
 	/**
 	 * 开发者注册后系统自动生成的TOKEN，可在官网登录后查看
 	 */
-	public static final String AUTH_TOKEN = "616fecfa234d61d6c5bc88364941960e";
+	public static final String AUTH_TOKEN = "";
 
 	/**
 	 * 响应数据类型, JSON或XML
