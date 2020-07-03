de9e883c645aec99a05bfed0894772c146a6020b
KiwiJuiceZ <43952178+KiwiJuiceZ@users.noreply.github.com>
2020-07-03 10:25:31 +0800

    Update TencentaiInfo.java

--- src/main/java/com/AISubtitles/Server/utils/tencentai/TencentaiInfo.java
+++ src/main/java/com/AISubtitles/Server/utils/tencentai/TencentaiInfo.java
@@ -2,7 +2,7 @@ package com.AISubtitles.Server.utils.tencentai;
 
 public class TencentaiInfo {
 
-    public final static String secretId = "AKIDOtAlnatOK1SwFBjQgj169Fhu1xnPmw5x";
-    public final static String secretKey = "sUr2zKPFyzkFWzHMZYIm00xeXCcsugCt";
+    public final static String secretId = "";
+    public final static String secretKey = "";
 
 }
